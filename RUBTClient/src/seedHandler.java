08eb648b06822f83049711ebb4760e8e2d50a52e
CMcD <CMcD@CMcD-PC>
2015-12-08 01:22:45 -0500

    moved listener method inside run()

--- RUBTClient/src/seedHandler.java
+++ RUBTClient/src/seedHandler.java
@@ -1,12 +1,21 @@
 import java.io.*;
 import java.net.*;
 
+/**
+ * This class is the thread running on top of our
+ * client that is listening to incoming leaches or 
+ * peers that are looking to download some of our
+ * file. Once the socket is establish a new Seed
+ * object is created.
+ * 
+ * @author Chris McDonough
+ */
 
 public class seedHandler implements Runnable {
 
 	ServerSocket seederListen;
 	
-	int portNum = 69420;
+	int portNum = 69420;	//may want to have this inputed as a argument to the constructor
 	
 	
 	public seedHandler(){
@@ -19,24 +28,24 @@ public class seedHandler implements Runnable {
 		
 	}
 	
-	public void listener() throws Exception {
-		try{
-			while(true){
-				Socket clientSocket = seederListen.accept();
-				//now we have a peer connected to us on this socket
-				//create a new seed for it
-				
-			}
-		} catch ( Exception e ){
-			e.printStackTrace();
-		}
-	}
+	
+	
+	/**
+	 * The running thread that waits and listens for 
+	 * incoming peers.
+	 */
 	
 	public void run(){
 		try{
-			listener();
-		}catch ( Exception e ){
-			e.printStackTrace();
+				while(true){
+					Socket clientSocket = seederListen.accept();
+					//now we have a peer connected to us on this socket
+					//create a new seed for it
+					
+				}
+		} catch ( Exception e ){
+				e.printStackTrace();
 		}
+		
 	}
 }
